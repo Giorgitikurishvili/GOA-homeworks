8232837f8a9169cb53cdc115979c6c226e03a04c
Giorgitikurishvili <giorgatikuragiorga@gmail.com>
2024-05-17 20:14:22 +0400

    second commit

---
+++ level 01/home work/lesson1.py
@@ -0,0 +1,59 @@
+name = "Andria "
+surname = "Lejava"
+print(name + surname)
+
+name = "Giorgi "
+surname = "Shavadze"
+print(name + surname)
+
+name = "Gio "
+surname = "Gagnidze"
+print(name + surname)
+
+name = "Tato "
+surname = "Buxrashvili"
+print(name + surname)
+
+name = "Luka "
+surname = "Oqruashvili"
+print(name + surname)
+
+name = "Shalva "
+surname = "Shubitidze"
+print(name + surname)
+
+name = "Tazo "
+surname = "Gogishvili"
+print(name + surname)
+
+name = "Zura "
+surname = "Gvalia"
+print(name + surname)
+
+name = "Vano "
+surname = "Motiashvili"
+print(name + surname)
+
+name = "Saba "
+surname = "Isakadze"
+print(name + surname)
+
+name = "Sandro "
+surname = "Jalagonia"
+print(name + surname)
+
+name = "Mate "
+surname = "Beridze"
+print(name + surname)
+
+name = "Dachi "
+surname = "Dadiani"
+print(name + surname)
+
+name = "Dachi "
+surname = "Tavadze"
+print(name + surname)
+
+name = "Daniel "
+surname = "Abramiani"
+print(name + surname)
