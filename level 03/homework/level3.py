88b36b0f08e2c6bfff537e0a888e4bfce59c0e33
Giorgitikurishvili <giorgatikuragiorga@gmail.com>
2024-05-20 22:19:50 +0400

    second commit

---
+++ level 03/homework/level3.py
@@ -0,0 +1,69 @@
+name = "Gio "
+surname = "Tikurishvili, "
+age = "13, "
+hobby = "hobby-programming, "
+country = "Georgia."
+
+print(name + surname + age + hobby + country)
+
+text = "Gamarjoba me var Giorgi Tikurishvili, me vswavlob GOAshi da vmushaob imistvis rom momavali uketesi mqondes. Madloba  GOal oriented academys."
+print(text)
+
+print(100 + 100)
+print(100 + 90)
+print(100 + 80)
+print(100 + 70)
+print(100 + 60)
+print(100 + 50)
+print(100 + 40)
+print(100 + 30)
+print(100 + 20)
+print(100 + 10)
+
+print(200 - 100)
+print(200 - 90)
+print(200 - 80)
+print(200 - 70)
+print(200 - 60)
+print(200 - 50)
+print(200 - 40)
+print(200 - 30)
+print(200 - 20)
+print(200 - 10)
+
+print(10 * 10)
+print(10 * 20)
+print(10 * 30)
+print(10 * 40)
+print(10 * 50)
+print(10 * 60)
+print(10 * 70)
+print(10 * 80)
+print(10 * 90)
+print(10 * 100)
+
+print(8 // 2)
+print(10 // 2)
+print(15 // 2)
+print(20 // 2)
+print(25 // 2)
+print(30 // 2)
+print(35 // 2)
+print(40 // 2)
+print(50 // 2)
+print(55 // 2)
+
+print(15 ** 1)
+print(15 ** 2)
+print(15 ** 3)
+print(15 ** 4)
+print(15 ** 5)
+print(15 ** 6)
+print(15 ** 7)
+print(15 ** 8)
+print(15 ** 9)
+print(15 ** 10)
+
+text = "Madloba yuradgebistvis"
+print(text)
+
