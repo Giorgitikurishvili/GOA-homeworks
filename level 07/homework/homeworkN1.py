6d87efc46f773ce941f78745a6cbd557828f3b46
Giorgitikurishvili <giorgatikuragiorga@gmail.com>
2024-06-05 20:31:37 +0400

    second commit

---
+++ level 07/homework/homeworkN1.py
@@ -0,0 +1,11 @@
+#1) თქვენს თავზე შემოიტანეთ ინფორმაცია: სახელი, გვარი, ასაკი, ქვეყანა, საყვარელი მანქანა, საყვარელი ფერი და ფავორიტი რიცხვი, შემოტანილი მნიშვნელობებით ააწყვეთ ერთი დიდი წინადადება.
+
+name = input("What is your name ? : ")
+surname = input("What is your surname ? : ")
+age = input("How old are you ? : ")
+country = input("Where do you live ? : ")
+car = input("Whats your favourite car ? : ")
+color = input("Whats your favourite color ? : ") 
+number = input("What is your favourite number ? : ")
+
+print(name + " " + surname + " " + age + " " + country + " " + car + " " + color + " " + number)
