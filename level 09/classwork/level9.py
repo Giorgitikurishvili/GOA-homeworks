0e182381712608995e11826400f0c02bec40d8f0
Giorgitikurishvili <giorgatikuragiorga@gmail.com>
2024-06-12 18:17:02 +0400

    second commit

---
+++ level 09/classwork/level9.py
@@ -0,0 +1,17 @@
+# a = "basket"
+# b = "ball"
+
+
+name = 'guga'
+num = 10
+decimal = 10.550
+
+print(type(name))
+print(type(num))
+print(type(decimal))
+
+name = 'guga'
+num = 10
+decimal = 10.550
+
+print(decimal + num)
---
+++ level 09/homework/homeworkN3.py
@@ -0,0 +1,6 @@
+#3) დაწერეთ პითონის პროგრამა, რომელიც მოუწოდებს 
+# მომხმარებელს შეიყვანოს რიცხვი და შემდეგ დაბეჭდოს ამ რიცხვის ორმაგი.
+
+number=int(input("Sheiyvanet ricxvi:"))
+doublethenumber=number*2
+print(doublethenumber)
